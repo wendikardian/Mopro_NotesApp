2dd2d0ad8c0b20c7945bb8905bb1c5c5bf35263e
wendikardian <wendikardian@gmail.com>
2022-07-22 17:41:11 +0700

    first

---
+++ src/navigator/MainNavigator.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import NoteListScreen from '../screens/NoteListScreen'
+import AddNoteScreen from '../screens/AddNoteScreen'
+
+
+const Stack = createStackNavigator()
+
+const MainNavigator = () => {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="NoteList">
+                <Stack.Screen name="NoteList" component={NoteListScreen}  options={{headerShown : false}} />
+                <Stack.Screen name="CreateNote" component={AddNoteScreen}  options={{headerShown : false}} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default MainNavigator
---
+++ src/screens/NoteListScreen.js
@@ -0,0 +1,45 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+import {Icon} from 'react-native-elements'
+
+const NoteListScreen = (props) => {
+  const {navigation} = props;
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>Notes</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('CreateNote')} >
+          <Icon name="plus" type="antdesign" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  mainContainer : {
+    flex : 1,
+    backgroundColor: 'white'
+  }, headerContainer : {
+    padding: 18,
+    backgroundColor: 'moccasin',
+    justifyContent : 'center',
+    alignItems : 'center'
+  }, headerTitle : {
+    fontSize: 20,
+    padding: 8,
+    fontWeight: 'bold'
+  }, buttonContainer : {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+  }, addButton : {
+    backgroundColor : 'pink',
+    padding: 16,
+    borderRadius : 100
+  }
+})
+
+export default NoteListScreen
